fd283bf7fb099295997ba885cbcf07f5bb3c9545
HAFIS-DAVIES <hafis.davies01@gmail.com>
2025-07-12 20:53:41 -0600

    Initial commit

---
+++ security_questions.py
@@ -0,0 +1,5 @@
+print("Welcome to the 29 Ave Resident Security Question Authenticator")
+city = input("What city did you immigrate from? : \n" )
+pet = input("What is the name of your first pet ? : \n")
+
+print("Kindly confirm if the inouts are correct before submitting : \n " + city + " and " + pet)
